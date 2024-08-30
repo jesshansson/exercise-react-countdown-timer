a94d87af8713951fd796e8b5b2bc430d7bdce409
Jessica Hansson <jessica.hansson@hotmail.se>
2024-08-30 11:51:13 +0200

    better semantics

--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -1,17 +1,19 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, ReactElement } from "react";
 
-export function CountdownTimer() {
+export function CountdownTimer(): ReactElement {
   const [timeLeft, setTimeLeft] = useState<number>(60); //Återstående sekunder
   const [isActive, setIsActive] = useState<boolean>(false); //Om timern är igång
   const [inputTime, setInputTime] = useState<string>(""); //Lagrar värdet som användaren skriver in i inputfälte
+  const [feedback, setFeedback] = useState<string>("");
   const timerRef = useRef<number | null>(null); // Referens för timer-id
 
   useEffect(() => {
-    if (isActive) {
+    if (isActive === true) {
       timerRef.current = setInterval(() => {
         setTimeLeft((prevTimeLeft) => {
           if (prevTimeLeft <= 1) {
             //När räkningen når 0
+            setFeedback("Bra jobbat!");
             clearInterval(timerRef.current!); // Stoppar intervallet (timern). "!" innebär att timerRef.current inte är null vid den tidpunkten (TypeScript behöver ej varna för fel).
             timerRef.current = null;
             setIsActive(false); // Säkerställ att timern stannar och att användaren inte kan starta den igen utan att återställa först
@@ -40,6 +42,7 @@ export function CountdownTimer() {
   const resetTimer = () => {
     setIsActive(false);
     setTimeLeft(60);
+    setFeedback("");
   };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -47,7 +50,8 @@ export function CountdownTimer() {
   };
 
   //Funktion som körs när användaren klickar på "Sätt tid"
-  const setTimer = () => {
+  const setTimer = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // Förhindra formuläret från att skicka och ladda om sidan
     const time = parseInt(inputTime, 10); // Konvertera inputvärdet till ett nummer, heltal (från en sträng)
     if (time > 0) {
       setTimeLeft(time); // Sätt timeLeft till användarens angivna tid
@@ -59,7 +63,7 @@ export function CountdownTimer() {
     <div className="counter-container">
       <h1>Hur många armhävningar klarar du på 1 minut?</h1>
       {timeLeft === 0 ? (
-        <h2>Bra jobbat!</h2>
+        <h2> {feedback}</h2>
       ) : (
         <h2 className="time-left">{timeLeft} sekunder kvar</h2>
       )}
@@ -68,7 +72,7 @@ export function CountdownTimer() {
         <button onClick={pauseTimer}>Pausa</button>
         <button onClick={resetTimer}>Återställ</button>
       </div>
-      <div className="set-time">
+      <form onSubmit={setTimer} className="set-time">
         <p>Testa med en egen tid!</p>
         <input
           type="number"
@@ -76,8 +80,8 @@ export function CountdownTimer() {
           onChange={handleInputChange}
           placeholder="Ange tid i sekunder"
         />
-        <button onClick={setTimer}> Sätt tid </button>
-      </div>
+        <button type="submit"> Sätt tid </button>
+      </form>
     </div>
   );
 }
